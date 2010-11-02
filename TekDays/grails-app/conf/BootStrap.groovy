bd54d24d4808334e5226c8330a41cf3583d8a762
hxj-rent <lost_alien@qq.com>
2010-11-02 22:37:17 +0800

    include a TekDays instance

--- TekDays/grails-app/conf/BootStrap.groovy
+++ TekDays/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import tekdays.TekEvent
+
 class BootStrap {
 
     def init = { servletContext ->
+		def event1 = new TekEvent(name:'QuakeCon',
+					city:'Beijing',
+					organizer:'sina',
+					venue:'shuangjing',
+					startDate: new Date('11/1/2010'),
+					endDate: new Date('11/3/2010'),
+					description:'welcome you')
+		if(!event1.save()){
+			println '**** error when saveing event1'
+		}
     }
     def destroy = {
     }
