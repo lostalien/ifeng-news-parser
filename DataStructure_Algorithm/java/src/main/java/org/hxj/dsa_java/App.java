5a870dd587348d287ede17a2fbdeedaacd46e2ab
lostalien <ilostalien@gmail.com>
2013-05-18 21:30:10 +0800

    新增了 整数的桶排序

--- DataStructure_Algorithm/java/src/main/java/org/hxj/dsa_java/App.java
+++ DataStructure_Algorithm/java/src/main/java/org/hxj/dsa_java/App.java
@@ -1,13 +1,25 @@
 package org.hxj.dsa_java;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<String, String>( );
+        map.put("name","a");
+        map.put("addr","b");
+        map.put("height","c");
+
+        //map.get("height");
+        Map<String,String> lm = new LinkedHashMap<String, String>(map);
+        Set<String> keys = lm.keySet();
+        for( String k : keys ){
+            System.out.println(k);
+        }
     }
 }
