8f8a2d919007d3f089b1ffcb2719404536f02147
lost_alien <lost_alien@qq.com>
2015-08-31 22:50:50 +0800

    Revert "注释修改"
    This reverts commit 8c5e33a1f6dd95fc62d0328d97314e6e2382f510.

--- DataStructure_Algorithm/java/src/test/java/org/hxj/dsa_java/AppTest.java
+++ DataStructure_Algorithm/java/src/test/java/org/hxj/dsa_java/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple Apps.
+ * Unit test for simple App.
  */
 public class AppTest 
     extends TestCase
