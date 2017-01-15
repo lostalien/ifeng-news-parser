f8cd8d40fc1566b35d9e669c83af209d979ea34f
redemption9527 <ihealthyhappy@qq.com>
2017-01-15 17:44:01 +0800

    新增了两个类

---
+++ ComeToVaadinAgain/tutorial-step3/src/main/java/my/vaadin/scims/NavigatorUI.java
@@ -0,0 +1,24 @@
+package my.vaadin.scims;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by hxj on 2017/1/15.
+ */
+public class NavigatorUI extends UI {
+
+    Navigator navigator;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        getPage().setTitle("Navigation Example");
+        // Create a navigator to control the views
+        navigator = new Navigator(this, this);
+        // Create and register the views
+        navigator.addView("", new LoginView(navigator));
+        navigator.addView("main", new MainView());
+    }
+}
